53918a5fbe2187efe8e9d522ce574ee94a48b64e
Swapnil Bhasale <bhasaless007@gmail.com>
2023-05-17 10:42:30 -0700

    production db change

--- database.py
+++ database.py
@@ -6,8 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 # Connect to PostgreSQL
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
-
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://addnysdm:L7rbJqKRCnKtrPUPZydP5yf3wqoQjBDb@mahmud.db.elephantsql.com/addnysdm'
 # this line is for SQLite only
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
